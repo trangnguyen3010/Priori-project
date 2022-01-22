843723a65eff69100aea6ea87a1fc3628b8c3bbd
Thai <thaila71669@gmail.com>
2022-01-22 01:15:50 -0600

    Added test post methods

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -7,12 +6,28 @@ function App() {
     .then(res => res.json())
     .then(data => console.log(data))
   }
+
+  const testData = {info : "testString"}
+  function sendData(){
+    fetch("/api/newData", {
+      method: "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(testData)
+    })
+    .then((res) => res.json())
+    .then((data) => console.log("Sent data"))
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <p>Open browser console and click the button below. Console should print "testing stuff" on click</p>
         <button onClick={testFn}>Test button</button>
 
+        <button onClick={sendData}>Click to send data to back end</button>
       </header>
     </div>
   );
--- server/server.js
+++ server/server.js
@@ -1,10 +1,21 @@
 const express = require("express");
+const bodyparser = require("body-parser");
 const app = express();
 const port = 5000;
+
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(bodyparser.json());
+
 
 app.get("/api/test", (req, res) => {
     res.json("testing stuff");
 });
 
+
+app.post("/api/newData", (req, res) => {
+
+    console.log(req.body);
+    res.json("Data received");
+})
 
 app.listen(port, () => {console.log(`Server on port ${port}`)})
